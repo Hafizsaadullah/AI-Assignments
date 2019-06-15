db260fbf6a3bc60675726836506a790c9fe5a2be
Hafiz Saad Ullah <hafizsaadullah@hotmail.com>
2019-06-16 02:23:07 +0500

    Removed Duplicate Number with for loop

---
+++ Removing_Duplicate.py
@@ -0,0 +1,17 @@
+# list = [1,2,3,4,1]
+# result = []
+# for i in list:
+#     if i not in result:
+#         result.append(i)
+
+
+
+
+
+list = [1,2,3,4,1,2,3,4,6,8,9,10]
+result = []
+for i in list:
+    if i not in result:
+        result.append(i)
+
+print("This is my list: ", result)
